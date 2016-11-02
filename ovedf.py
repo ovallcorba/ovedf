cd7c9d0eac06d5067e7df62a32feeae02e1b990f
ori <ori@vava33.com>
2016-11-02 11:45:00 +0100

    add counters on header. direct calc ESD npix. Speed opt

--- ovedf.py
+++ ovedf.py
@@ -7,7 +7,10 @@ Created on Mon Oct 17 12:00:00 2016
 
 ---------------
 Changelog
-Current (last change 161024 11.40h):
+Current (last change 161102 11.45h):
+ - Added counters on header
+ - Speed optimization, only histogram filling is pending
+ - direct calculation of ESD using npix
  - Azimuthal
  - Support for multiple files (batch)
  - Header as in ffops generated files
@@ -19,11 +22,10 @@ Current (last change 161024 11.40h):
  - Reading EDF and INP files. IntegOpts.
 
 TODO:
- - fix esd (it is for q nm-1)
- - Speed up
+ - Speed up histogram filling
  - MASKS
- - xbin, ybin -- do not get what they do (it is not the same as reducing radial bins??)
- - geometrical corrections needed?
+ - xbin, ybin -- not necessary
+ - check geometrical corrections?
 
 """
 
@@ -46,6 +48,9 @@ class EDFdata:
         self.wavelA = 0.4246
         self.xcen = 1024
         self.ycen = 1024
+        self.comment = ''
+        self.counter_mne = []
+        self.counter_pos = []
         # these data will be retrieved from the edf header
         
         self.t2XY = None   #np array of t2
@@ -100,6 +105,15 @@ class EDFdata:
                 if line.startswith("ref_wave"):
                     self.wavelA = float(line[iigual+1:ipcoma].strip())
                     log.debug("ref_wave="+str(self.wavelA))
+                if line.startswith("Comment"):
+                    self.comment = line[iigual+1:ipcoma].strip()
+                    log.debug("Comment="+self.comment)
+                if line.startswith("counter_mne"):
+                    self.counter_mne = line[iigual+1:ipcoma].strip().split()
+                    log.debug("counter_mne="+str(self.counter_mne))
+                if line.startswith("counter_pos"):
+                    self.counter_pos = line[iigual+1:ipcoma].strip().split()
+                    log.debug("counter_pos="+str(self.counter_pos))
                 lcount=lcount+1
         finally:
             log.debug("finally executed")
@@ -122,9 +136,6 @@ class EDFdata:
         header = fedf.read(headersize)
         log.debug(header)
         try:
-#            for y in range(self.dimy):
-#                for x in range(self.dimx):
-#                    self.intenXY[y,x] =  struct.unpack("<H",fedf.read(2))[0] #edf I as unsigned short little endian
             self.intenXY = np.fromfile(fedf,np.uint16)
             self.intenXY = self.intenXY.reshape(self.dimx,self.dimy)
         finally:
@@ -169,11 +180,16 @@ class IntegOptions:
         try:
             for line in finp:
                 log.debug(line)
+                if line.strip().startswith("#"): continue
                 try:
                     iigual = line.index("=")
-                    ipcoma = line.index(";")
                 except:
                     continue
+                try:
+                    ipcoma = line.index(";")
+                except:
+                    ipcoma = len(line)-1
+
                 if line.startswith("X-BEAM"):
                     self.xcen = float(line[iigual+1:ipcoma].strip()) 
                     log.debug("X-BEAM="+str(self.xcen))
@@ -251,7 +267,7 @@ def calc2tDeg(integOpts, edfdata, rowY,colX):
     sintilt = integOpts.sintilt
     cosrot = integOpts.cosrot
     sinrot = integOpts.sinrot
-    distPix = (integOpts.distMDmm/edfdata.pixSXmm)#/integOpts.costilt
+    distPix = (integOpts.distMDmm/edfdata.pixSXmm)/integOpts.costilt
 
     # vector centre-pixel
     vPCx=(float)(colX)-integOpts.xcen
@@ -276,17 +292,15 @@ def getAzim(integOpts,edfdata,rowY,colX):
     if (azim<0): azim = azim + 2*math.pi
     return math.degrees(azim)
 
-def calc2tDegFullFASTER(integOpts, edfdata): #and azimuth
+def calc2tDegFull(integOpts, edfdata): #and azimuth
     """full image calculation (to speed up using numpy operations)"""
     t0 = time.time()
     #inits
     vCPy,vCPx = np.mgrid[0:edfdata.dimy,0:edfdata.dimx]
     vCPz = np.empty([edfdata.dimx,edfdata.dimy])
-    distPix = (integOpts.distMDmm/edfdata.pixSXmm)
-    #horizontal vector (for the azimuth)
-    horX = 1.0
-    horY = 0.0
+    distPix = (integOpts.distMDmm/edfdata.pixSXmm)/integOpts.costilt
     
+    #vectors calculation
     vCPy = integOpts.ycen - vCPy
     vCPx = vCPx - integOpts.xcen
     vCPz = (-vCPx*integOpts.sinrot + vCPy*integOpts.cosrot)*(-integOpts.sintilt)
@@ -295,63 +309,8 @@ def calc2tDegFullFASTER(integOpts, edfdata): #and azimuth
     
     #Azim calc
     t0 = time.time()
-    edfdata.azim = np.degrees(np.arctan2(vCPx,vCPy))
-    log.debug(" (y,x)=1200,1200 azim = %.2f deg"%(edfdata.azim.item(1200,1200)))
-    edfdata.azim=np.where(edfdata.azim < 0, edfdata.azim+360,edfdata.azim)
-    log.info(" azim calculation: %.4f sec"%(time.time() - t0))
-    log.debug(" (y,x)=1200,1200 azim = %.2f deg"%(edfdata.azim.item(1200,1200)))
-    log.debug(" (y,x)=1200,800 azim = %.2f deg"%(edfdata.azim.item(1200,800)))
-    log.debug(" (y,x)=800,1200 azim = %.2f deg"%(edfdata.azim.item(800,1200)))
-    log.debug(" (y,x)=800,800 azim = %.2f deg"%(edfdata.azim.item(800,800)))
-    
-    #T2 CALC (modulus, arctan, geomCorr, todegrees)
-    t0 = time.time()
-    edfdata.t2XY = vCPx**2 + vCPy**2 - vCPz**2
-    edfdata.t2XY = np.sqrt(edfdata.t2XY)
-    log.info(" sqrt calculation: %.4f sec"%(time.time() - t0))
-
-    t0 = time.time()
-    edfdata.t2XY = edfdata.t2XY/ (distPix-vCPz)
-    edfdata.t2XY = np.arctan(edfdata.t2XY)
-    log.info(" atan calculation: %.4f sec"%(time.time() - t0))
-    
-    #TODO: GEOM CORRECTIONS 
-    #t0 = time.time()
-    #Add Something if necessary:
-    #edfdata.gcorr = 1/((np.cos(edfdata.t2XY))**2)  #lorentz correction pixels
-    #incidence angle correction:
-    #edfdata.gcorr = edfdata.t2XY - (math.radians(integOpts.angleTilt)*(np.sin(np.radians(edfdata.azim)-math.radians(integOpts.tiltRotation))))  #incidence angle effective
-    #edfdata.gcorr = 1/(np.cos(edfdata.gcorr)*np.cos(edfdata.gcorr)*np.cos(edfdata.gcorr))
-    #log.info(" gcorr calculation: %.4f sec"%(time.time() - t0))
-    
-    t0 = time.time()
-    edfdata.t2XY = np.degrees(edfdata.t2XY)
-    log.info(" to deg calculation: %.4f sec"%(time.time() - t0))
-
-
-def calc2tDegFull(integOpts, edfdata): #and azimuth
-    """full image calculation (to speed up using numpy operations)"""
-    #first fill up useful arrays (e.g. vPCx**2+vPCy**2-zcomponent**2)
-    t0 = time.time()
-    #inits
-    vCPx = np.empty([edfdata.dimx,edfdata.dimy])
-    vCPy = np.empty([edfdata.dimx,edfdata.dimy])
-    vCPz = np.empty([edfdata.dimx,edfdata.dimy])
-    distPix = (integOpts.distMDmm/edfdata.pixSXmm)
-    #horizontal vector (for the azimuth)
-    horX = 1.0
+    horX = 1.0  #horizontal vector
     horY = 0.0
-    for y in range(edfdata.dimy):
-        cpy = integOpts.ycen-(float)(y)
-        for x in range(edfdata.dimx):
-            cpx = (float)(x)-integOpts.xcen
-            vCPx[y,x] = cpx
-            vCPy[y,x] = cpy
-            vCPz[y,x] = (-cpx*integOpts.sinrot + cpy*integOpts.cosrot)*(-integOpts.sintilt)
-    log.info(" vector calculation: %.4f sec"%(time.time() - t0))
-    
-    #Azim calc
-    t0 = time.time()
     edfdata.azim = np.degrees(np.arctan2(vCPx,vCPy))
     log.debug(" (y,x)=1200,1200 azim = %.2f deg"%(edfdata.azim.item(1200,1200)))
     edfdata.azim=np.where(edfdata.azim < 0, edfdata.azim+360,edfdata.azim)
@@ -379,7 +338,7 @@ def calc2tDegFull(integOpts, edfdata): #and azimuth
 ######################################################## MAIN PROGRAM
 if __name__=="__main__":
 
-    __version__ = '161021'
+    __version__ = '161102'
     
     ts = time.time()
     st = '[' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') + '] '
@@ -388,11 +347,12 @@ if __name__=="__main__":
     #argument parsing
     parser = argparse.ArgumentParser(description="EDF image integration")
     parser.add_argument('filenames', nargs='*', help='edf file')
-    parser.add_argument("-i", "--par", default=None, nargs=1, help='calibration input file')
-    parser.add_argument("-o", "--output", default="", nargs=1, help='1D output file only when processing one file (default: filename.dat')
-    parser.add_argument("-s", "--suffix", default="", nargs=1, help='suffix for the output names (before the dat extension)')
+    parser.add_argument("-i", "--par", default=None, help='calibration input file')
+    parser.add_argument("-o", "--output", default="", help='1D output file only when processing one file (default: filename.dat')
+    parser.add_argument("-s", "--suffix", default="", help='suffix for the output names (before the dat extension)')
+    parser.add_argument("-ne", "--noesd", default=False, help='do not calculate esd\'s')
     parser.add_argument("-pl", "--plot", action="store_true", help='plot image')
-    parser.add_argument("-d", "--debug", action="count", help='debug mode')    
+    parser.add_argument("-d", "--debug", action="count", help='debug mode')
     args = parser.parse_args()
     
     #print args.debug
@@ -419,24 +379,24 @@ if __name__=="__main__":
 
     outFiles = []
     if (len(args.filenames)==1):
-        if args.output[0] == "":
+        if args.output == "":
             ipunt = args.filenames[0].rfind(".")
-            outFiles.append(args.filenames[0][0:ipunt]+args.suffix[0]+".dat")
+            outFiles.append(args.filenames[0][0:ipunt]+args.suffix+".dat")
         else:
-            outFiles.append(args.output[0])
+            outFiles.append(args.output)
     else:
         #same names as input but with dat extension
         for fname in files:
             ipunt = fname.rfind(".")
-            outFiles.append(fname[0:ipunt]+args.suffix[0]+".dat")
+            outFiles.append(fname[0:ipunt]+args.suffix+".dat")
     log.info("output file(s): %s"%outFiles)
     print "  Output file(s): %s"%(outFiles)
    
     #now we read the calibration parameter file
     integOpts = IntegOptions()
     if args.par is not None: #otherwise default parameters (image headers) will be used
-        log.info("reading inp file:"+args.par[0])
-        integOpts.readINPFile(args.par[0])
+        log.info("reading inp file:"+args.par)
+        integOpts.readINPFile(args.par)
         
         #put start and end azim in range 0-360 starting at 12o'clock direction clockwise !!
         #Inserted as fit2d 3o'clock=zero and positive clockwise (because it is y inverted!)
@@ -469,12 +429,8 @@ if __name__=="__main__":
         #Integration options
         t2in = calc2tDeg(integOpts, edf, integOpts.ycen,integOpts.xcen+integOpts.inRadi)
         t2fin = calc2tDeg(integOpts, edf, integOpts.ycen,integOpts.xcen+integOpts.outRadi)
-        #TODO azimbins
         size = (int)(integOpts.radialBins)
         step = (t2fin-t2in)/size
-        #correct t2in t2fin to be at the "centre" of the bin
-#        t2in = t2in #+ step/2
-#        t2fin = t2fin - step #correction to get the nbins and no nbins+1
         
         log.info("Processing %s"%(fname))
         log.info("t2in=%f t2f=%f size=%d step=%f"%(t2in,t2fin,size,step))
@@ -490,7 +446,7 @@ if __name__=="__main__":
         log.info(" startAzim endAzim internally used used (ref. 12h CW+) = %f %f"%(startAzim,endAzim))
     
         #HEAVY CALCULATION
-        calc2tDegFullFASTER(integOpts,edf)
+        calc2tDegFull(integOpts,edf)
     
         #IN CASE WE WANT TO PLOT THE IMAGE (I moved it after calculations)
         if (args.plot):
@@ -589,13 +545,15 @@ if __name__=="__main__":
                 ipunt = iniName.rfind(".")
                 outFiles[i] = iniName[0:ipunt]+"_bin%02d.dat"%(k)
             fout = open(outFiles[i], 'w')
-            fout.write("#%s \n"%(fname))
+            fout.write("#%s  COM = %s\n"%(fname,edf.comment))
             fout.write("#I vs. 2Theta [deg] Azim: %.2f %.2f Radial %d %d Tilts: %.2f %.2f Dist: %.2f Wave: %.4f Cen: %.2f %.2f Pix: %.2f Bin: %d\n"
                     %(integOpts.startAzim,integOpts.endAzim,integOpts.inRadi,integOpts.outRadi,integOpts.tiltRotationIN,integOpts.angleTilt,integOpts.distMDmm,integOpts.wavelA,integOpts.xcen,integOpts.ycen,edf.pixSXmm*1000,integOpts.azimBins))
             if (integOpts.azimBins>1):
                 fout.write("# Azim Bin %d: from %.2f to %.2f\n"%(k,azimIni[k],azimFin[k]))
-            else:
-                fout.write("# \n") #blank line (for plmany, it takes min 4 comment lines)
+            line = ''
+            for j in range(len(edf.counter_mne)):
+              line = line + ' ' + edf.counter_mne[j] + ' ' + edf.counter_pos[j]
+            fout.write("# %s\n"%(line))
             fout.write("# %d \n"%(size)) #number of points
             for j in range(size):
                 if patts[k].npix[j] <= 0:continue
@@ -609,6 +567,7 @@ if __name__=="__main__":
                 if (icorr<0): icorr = 0
                 inten = icorr/patts[k].npix[j]
                 
+                #esd (direct calculation using npix)
                 if (args.noesd == False):
                     esd = math.sqrt(inten/patts[k].npix[j])
                     #write the line
